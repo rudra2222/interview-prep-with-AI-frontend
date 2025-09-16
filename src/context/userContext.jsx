e80c59dc89d1301927168905c652a7ca024419ec
rudra <rudravekariya020@gmail.com>
2025-09-17 00:08:17 +0530

    the update in context and dashboard

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,5 +1,5 @@
 import React , {createContext , useState , useEffect} from "react";
-import axiosInstance from "../utils/axiosInstance.js";
+import axiosInstance from "../utils/axiosInstance";
 import {API_PATHS} from "../utils/apiPaths";
 
 export const UserContext = createContext();
